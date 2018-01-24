465d67368b0071181983e893a39455a2b387d278
Maciej Gorzala <maciej.gorzala@gmail.com>
2018-01-24 18:12:49 +0100

    init commit

---
+++ src/main/java/com/bestapp/companymanage/CompanyManageApplication.java
@@ -0,0 +1,12 @@
+package com.bestapp.companymanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyManageApplication.class, args);
+	}
+}
